3525969ae6f905a2064a5114aaea50367904da5c
SHIN <akiyachiumi@gmail.com>
2025-07-10 22:51:12 +0800

    feat: add lib:build command

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,10 +1,11 @@
+import { sayHello } from '@packages/lib';
 import express from 'express';
 
 const app = express();
 const port = process.env.PORT ?? '9001';
 
 app.get('/', (_, res) => {
-  res.send('Hello World!');
+  res.send(sayHello('Lib Test'));
   console.log('Response sent');
 });
 
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
+import { TestLibComponent } from './test-lib';
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <TestLibComponent />
       <main className={styles.main}>
         <Image
           className={styles.logo}
