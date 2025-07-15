20a1b000bb7779a64051ca4c5f0b712d51d30f2e
SHIN <akiyachiumi@gmail.com>
2025-07-15 23:33:03 +0800

    feat: update navbar layout

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import MessageBox from '@/components/MessageBox';
 import { TestComp } from '@/components/TestComp';
 import { AppShell, Burger, Flex, Stack } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
@@ -11,28 +10,21 @@ export default function Home() {
   return (
     <AppShell
       navbar={{
-        width: 300,
-        breakpoint: 'sm',
+        width: 280,
+        breakpoint: 0,
         collapsed: { mobile: !opened, desktop: !opened },
       }}
       padding="md"
+      layout="alt"
     >
       <AppShell.Header bg="transparent" bd="none">
-        <Burger opened={opened} onClick={toggle} size="sm" m="16" />
+        <Burger opened={opened} onClick={toggle} size="lg" m="16" />
       </AppShell.Header>
 
       <AppShell.Navbar p="md">
         <Flex justify="space-between" align="center">
           <div>Cozy Chat</div>
-          <Burger
-            opened={opened}
-            onClick={toggle}
-            size="sm"
-            style={{ margin: '16px' }}
-          />
         </Flex>
-
-        {/* Navbar content */}
       </AppShell.Navbar>
 
       <AppShell.Main
@@ -43,12 +35,8 @@ export default function Home() {
         }}
       >
         <Stack maw={480} mx="auto" bg="gray" gap="md">
-          <MessageBox isUser={false} message="Hello" created_at="2021-01-01" />
-          <MessageBox
-            isUser={true}
-            message="HelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHelloHello"
-            created_at="2021-01-01"
-          />
+          <TestComp />
+          <TestComp />
         </Stack>
       </AppShell.Main>
     </AppShell>
