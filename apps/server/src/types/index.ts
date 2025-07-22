24705b4c706a7e95818dd33e2aca92f03e15b366
Shin <penspulse@gmail.com>
2025-07-22 18:00:00 +0800

    feat: separate socket event logic

---
+++ apps/server/src/types/index.ts
@@ -0,0 +1,7 @@
+export enum SocketEvent {
+  MATCH_CANCEL = 'match:cancel',
+  MATCH_FAIL = 'match:fail',
+  MATCH_START = 'match:start',
+  MATCH_SUCCESS = 'match:success',
+  SEND_MESSAGE = 'send:message',
+}
