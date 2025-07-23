8d19a209197f8a28fd46888991d67dc9e555dc51
Shin <penspulse@gmail.com>
2025-07-23 14:01:13 +0800

    feat: add match timeout logic

--- apps/server/src/services/socket-server.service.ts
+++ apps/server/src/services/socket-server.service.ts
@@ -1,9 +1,9 @@
-import type { Server, Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 
-import { SocketEvent } from '@/types';
+import { CHAT_EVENT } from '@/types';
 
 class SocketServerService {
-  private readonly waitingUsers: string[] = [];
+  private waitingUsers: string[] = [];
 
   constructor(private readonly io: Server) {
     io.on('connection', (socket: Socket) => {
@@ -11,15 +11,15 @@ class SocketServerService {
     });
   }
 
-  onClientConnect(socket: Socket) {
+  private onClientConnect(socket: Socket) {
     const newUserId = socket.id;
     console.log('新的用戶連線:', newUserId);
 
-    socket.on(SocketEvent.MATCH_START, () => {
-      this.handleMatchRequest(socket);
+    socket.on(CHAT_EVENT.MATCH_START, () => {
+      this.handleMatchRequest(socket.id);
     });
 
-    socket.on(SocketEvent.MESSAGE_SEND, ({ message, roomId }) => {
+    socket.on(CHAT_EVENT.MESSAGE_SEND, ({ message, roomId }) => {
       this.sendMessageToRoom(message, roomId);
     });
 
@@ -28,35 +28,49 @@ class SocketServerService {
     });
   }
 
-  handleMatchRequest(socket: Socket) {
-    const newUserId = socket.id;
-
-    // 無人等待時加入 pool
+  private handleMatchRequest(newUserId: string) {
+    // 無人等待時加入等待池
     if (this.waitingUsers.length === 0) {
-      this.waitingUsers.push(newUserId);
-      console.log(`找不到等待中的用戶，${newUserId} 加入等待池`);
-      console.log('waitingUsers', this.waitingUsers);
+      this.addWaitingUser(newUserId);
       return;
     }
 
-    const peerUserId = this.waitingUsers.shift()!;
+    // 從等待池中移除用戶
+    const peerUserId = this.waitingUsers.shift();
+
+    if (!peerUserId) {
+      this.addWaitingUser(newUserId);
+      return;
+    }
 
     // 配對成功
-    this.processMatchSuccess(socket, newUserId, peerUserId);
+    this.processMatchSuccess(newUserId, peerUserId);
   }
 
-  async processMatchSuccess(
-    socket: Socket,
-    newUserId: string,
-    peerUserId: string
-  ) {
+  private addWaitingUser(newUserId: string) {
+    this.waitingUsers.push(newUserId);
+    console.log(`找不到等待中的用戶，${newUserId} 加入等待池`);
+
+    setTimeout(() => {
+      if (this.waitingUsers.includes(newUserId)) {
+        this.waitingUsers = this.waitingUsers.filter(
+          (userId) => userId !== newUserId
+        );
+
+        console.log(`${newUserId} 等待超時，從等待池移除`);
+        this.io.of('/').sockets.get(newUserId)?.emit(CHAT_EVENT.MATCH_FAIL);
+      }
+    }, 3000);
+  }
+
+  private async processMatchSuccess(newUserId: string, peerUserId: string) {
     const roomId = `room-${peerUserId}-${newUserId}`;
 
-    await socket.join(roomId);
+    await this.io.of('/').sockets.get(newUserId)?.join(roomId);
     await this.io.of('/').sockets.get(peerUserId)?.join(roomId);
 
     // 通知雙方配對成功，附帶 roomId
-    this.io.to(roomId).emit('match:success', {
+    this.io.to(roomId).emit(CHAT_EVENT.MATCH_SUCCESS, {
       userIds: [newUserId, peerUserId],
       roomId,
     });
@@ -64,11 +78,11 @@ class SocketServerService {
     console.log(`配對成功: ${peerUserId} <-> ${newUserId} 房間 ID: ${roomId}`);
   }
 
-  sendMessageToRoom(message: string, roomId: string) {
-    this.io.to(roomId).emit(SocketEvent.MESSAGE_RECEIVE, message);
+  private sendMessageToRoom(message: string, roomId: string) {
+    this.io.to(roomId).emit(CHAT_EVENT.MESSAGE_RECEIVE, message);
   }
 
-  onClientDisconnect(socket: Socket) {
+  private onClientDisconnect(socket: Socket) {
     const userId = socket.id;
     console.log('用戶斷開連線:', userId);
   }
--- apps/server/src/types/index.ts
+++ apps/server/src/types/index.ts
@@ -1,12 +1,12 @@
-export enum SocketEvent {
-  MATCH_START = 'match:start',
-  MATCH_SUCCESS = 'match:success',
-  MATCH_CANCEL = 'match:cancel',
-  MATCH_FAIL = 'match:fail',
+export const CHAT_EVENT = {
+  MATCH_START: 'match:start',
+  MATCH_SUCCESS: 'match:success',
+  MATCH_CANCEL: 'match:cancel',
+  MATCH_FAIL: 'match:fail',
 
-  MESSAGE_SEND = 'message:send',
-  MESSAGE_RECEIVE = 'message:receive',
-}
+  MESSAGE_SEND: 'message:send',
+  MESSAGE_RECEIVE: 'message:receive',
+};
 
 export type ChatMessage = {
   message: string;
