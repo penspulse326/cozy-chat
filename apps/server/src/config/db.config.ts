91bc7de2d18fad162690178268883a3f1224585f
Shin <penspulse@gmail.com>
2025-07-23 15:42:02 +0800

    feat: add db config

---
+++ apps/server/src/config/db.config.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+
+async function connectDB() {
+  try {
+    mongoose.connect(
+      process.env.MONGODB_URI || 'mongodb://localhost:27017/cozychat'
+    );
+    console.log('DB 連線成功！');
+  } catch (error) {
+    console.error('DB 連線錯誤：', error);
+    process.exit(1);
+  }
+}
+
+async function disconnectDB() {
+  await mongoose.disconnect();
+  console.log('DB 已斷開連線。');
+}
+
+export { connectDB, disconnectDB };
--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -3,20 +3,22 @@ import http from 'http';
 import { dirname } from 'path';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
+import { connectDB } from './config/db.config';
 import SocketServerService from './services/socket-server.service';
 
+const port = process.env.PORT ?? '9001';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
-const port = process.env.PORT ?? '9001';
+new SocketServerService(new Server(server));
 
 app.get('/', (_, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-new SocketServerService(new Server(server));
+connectDB();
 
 server.listen(port, () => {
   console.log(`listening on *:${port}`);
