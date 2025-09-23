0a9c18ec16df771841151e53372bb55fc5b33d01
Shin <akiyachiumi@gmail.com>
2025-09-23 23:33:27 +0800

    chore: update cors url

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,28 +1,28 @@
 
 import express from 'express';
 import http from 'http';
-import { dirname } from 'path';
+// import { dirname } from 'path';
 import { Server } from 'socket.io';
-import { fileURLToPath } from 'url';
+// import { fileURLToPath } from 'url';
 
 import { connectToDB, disconnectFromDB } from '@/config/db';
 import { setupSocketServer } from '@/socket';
 
 const port = process.env.PORT ?? '9001';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (_, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+// app.get('/', (_, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 async function bootstrap() {
   const url =
     process.env.ENV === 'production'
-      ? 'https://cozy-chat.vercel.app'
+      ? 'https://cozy-chat-penspulse.vercel.app'
       : 'http://localhost:3000';
 
   try {
